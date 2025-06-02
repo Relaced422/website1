a70b0fddaf56c577d33544b2dab4a1ec0c4abc94
Relaced422 <optquenum@gmail.com>
2025-06-02 11:45:39 +0200

    First Commit

---
+++ includes/header.php
@@ -0,0 +1,12 @@
+<header>
+        <div class="header__logo">
+            <a href="/"><img src="/assets/img/logo.png" alt="Logo"></a>
+        </div>
+        <nav class="header__nav">
+            <ul>
+                <li><a href="/">Menu</a></li>
+                <li><a href="/about">admin</a></li>
+                <li><a href="/services">login</a></li>
+            </ul>
+        </nav>
+</header>
---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tobiets</title>
+    <link rel="stylesheet" href="assets/css/style.css"> <!-- Verwijzing naar CSS -->
+</head>
+<body>
+
+    <!-- Header -->
+    <div class="header">
+        <button>Menu</button>
+        <div class="center">Deze is verborgen</div>
+        <button>Admin</button>
+        <button>Login</button>
+    </div>
+
+    <!-- Sections -->
+    <button class="section-button">Voorgerecht ⌄</button>
+    <div class="menu-container">
+        <div class="menu-item">
+            <div class="image">Afbeelding</div>
+            <div class="info">
+                <div class="price">€9.99</div>
+                <p>Beschrijving van het product<br>Bla Bla Bla Bla Bla</p>
+            </div>
+        </div>
+        <div class="menu-item">
+            <div class="image">Afbeelding</div>
+            <div class="info">
+                <div class="price">€9.99</div>
+                <p>Beschrijving van het product<br>Bla Bla Bla Bla Bla</p>
+            </div>
+        </div>
+        <div class="menu-item">
+            <div class="image">Afbeelding</div>
+            <div class="info">
+                <div class="price">€9.99</div>
+                <p>Beschrijving van het product<br>Bla Bla Bla Bla Bla</p>
+            </div>
+        </div>
+        <div class="menu-item">
+            <div class="image">Afbeelding</div>
+            <div class="info">
+                <div class="price">€9.99</div>
+                <p>Beschrijving van het product<br>Bla Bla Bla Bla Bla</p>
+            </div>
+        </div>
+    </div>
+
+    <button class="section-button">Hoofdgerecht ⌄</button>
+    <button class="section-button">Snacks ⌄</button>
+    <button class="section-button">Desert ⌄</button>
+
+</body>
+
+</html>
+
+</body>
+
+</html>
